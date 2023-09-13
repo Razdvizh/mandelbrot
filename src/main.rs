8f618404fb3d6b5488c76dd8ee2bd8133b02aa58
Razdvizh <114196034+Razdvizh@users.noreply.github.com>
2023-09-13 15:25:30 -0500

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-#![warn(rust_2018_idioms)]
-#![allow(elided_lifetimes_in_paths)]
-
+use image::{ImageEncoder, ImageError, ColorType};
 use num::Complex;
-use rayon::prelude::*;
+use std::str::FromStr;
+use std::fs::File;
+use std::env;
 
 /// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
 /// iterations to decide.
@@ -12,19 +12,19 @@ use rayon::prelude::*;
 /// origin. If `c` seems to be a member (more precisely, if we reached the
 /// iteration limit without being able to prove that `c` is not a member),
 /// return `None`.
-fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
-    let mut z = Complex { re: 0.0, im: 0.0 };
-    for i in 0..limit {
-        if z.norm_sqr() > 4.0 {
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize>
+{
+    let mut z = Complex {re: 0.0, im: 0.0 };
+    for i in 0..limit
+    {
+        if z.norm_sqr() > 4.0
+        {
             return Some(i);
         }
         z = z * z + c;
     }
-
     None
 }
-
-use std::str::FromStr;
 
 /// Parse the string `s` as a coordinate pair, like `"400x600"` or `"1.0,0.5"`.
 ///
@@ -57,8 +57,6 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
 
-/// Parse a pair of floating-point numbers separated by a comma as a complex
-/// number.
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
     match parse_pair(s, ',') {
         Some((re, im)) => Some(Complex { re, im }),
@@ -111,54 +109,48 @@ fn test_pixel_to_point() {
 /// arguments specify points on the complex plane corresponding to the upper-
 /// left and lower-right corners of the pixel buffer.
 fn render(pixels: &mut [u8],
-          bounds: (usize, usize),
-          upper_left: Complex<f64>,
-          lower_right: Complex<f64>)
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>)
 {
-    assert!(pixels.len() == bounds.0 * bounds.1);
+assert!(pixels.len() == bounds.0 * bounds.1);
 
-    for row in 0..bounds.1 {
-        for column in 0..bounds.0 {
-            let point = pixel_to_point(bounds, (column, row),
-                                       upper_left, lower_right);
-            pixels[row * bounds.0 + column] =
-                match escape_time(point, 255) {
-                    None => 0,
-                    Some(count) => 255 - count as u8
-                };
-        }
-    }
+for row in 0..bounds.1 {
+  for column in 0..bounds.0 {
+      let point = pixel_to_point(bounds, (column, row),
+                                 upper_left, lower_right);
+
+        pixels[row * bounds.0 + column] =
+          match escape_time(point, 255) {
+              None => 0,
+              Some(count) => 255 - count as u8
+          };
+  }
 }
-
-use image::ColorType;
-use image::png::PNGEncoder;
-use std::fs::File;
+}
 
 /// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
 /// file named `filename`.
 fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
-    -> Result<(), std::io::Error>
+    -> Result<(), ImageError>
 {
     let output = File::create(filename)?;
-
-    let encoder = PNGEncoder::new(output);
-    encoder.encode(&pixels,
-                   bounds.0 as u32, bounds.1 as u32,
-                   ColorType::Gray(8))?;
+    
+    let encoder = image::codecs::png::PngEncoder::new(output);
+    encoder.write_image(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::L8)?;
 
     Ok(())
 }
 
-use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 5 {
         eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT",
-                  args[0]);
+                  &args[0]);
         eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
-                  args[0]);
+                  &args[0]);
         std::process::exit(1);
     }
 
@@ -171,25 +163,27 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    // Scope of slicing up `pixels` into horizontal bands.
+    let threads = num_cpus::get();
+    let rows_per_band = bounds.1 / threads + 1;
     {
-        let bands: Vec<(usize, &mut [u8])> = pixels
-            .chunks_mut(bounds.0)
-            .enumerate()
+        let bands: Vec<&mut [u8]> = pixels
+            .chunks_mut(rows_per_band * bounds.0)
             .collect();
+        std::thread::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate()
+            {
+                let top = rows_per_band * i;
+                let height = band.len() / (bounds.0);
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
 
-        bands.into_par_iter()
-            .for_each(|(i, band)| {
-                let top = i;
-                let band_bounds = (bounds.0, 1);
-                let band_upper_left = pixel_to_point(bounds, (0, top),
-                                                     upper_left, lower_right);
-                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + 1),
-                                                      upper_left, lower_right);
-                render(band, band_bounds, band_upper_left, band_lower_right);
-            });
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
     }
 
-    write_image(&args[1], &pixels, bounds)
-        .expect("error writing PNG file");
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
